3cebf05bd3be7c60a4651933ad7b75f282dd5609
chenchaoyou555 <chenchaoyou555@github.com>
2026-07-28 16:44:05 +0800

    FitPlan v2.6 - AI健身训练与饮食计划PWA

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'fitplan-v1';
+const FILES = [
+  './app.html',
+  './knowledge.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => {
+      if (e.request.mode === 'navigate') return caches.match('./app.html');
+    }))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
